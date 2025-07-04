d25838fd874a9e993a8e5a93a4cfe8f3ded66214
MeetAntala45 <meetantala36@gmail.com>
2025-07-04 08:36:59 +0530

    [Test 1.2] Added failing test for returning number for single numeric input

--- tests/StringCalculator.test.js
+++ tests/StringCalculator.test.js
@@ -10,4 +10,8 @@ describe("String Calculator", () => {
   test("should return 0 when input is an empty string", () => {
     expect(calculator.Add("")).toBe(0);
   });
+
+  test("should return the number itself when a single number is provided", () => {
+    expect(calculator.Add("5")).toBe(5);
+  });
 });

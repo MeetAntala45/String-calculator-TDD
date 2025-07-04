9817076c19689827101ae12f23cbd03bfe21633c
MeetAntala45 <meetantala36@gmail.com>
2025-07-04 09:04:24 +0530

    [Test 2] Refactored Add() method to simplify logic for summing any number of values

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -5,12 +5,6 @@ class StringCalculator {
     }
 
     const numbers = inputString.split(",").map(Number);
-    if (numbers.length === 1) {
-      return parseInt(numbers[0], 10);
-    } else if (numbers.length === 2) {
-      return parseInt(numbers[0] + numbers[1], 10);
-    }
-
     return numbers.reduce((sum, num) => sum + num, 0);
   }
 }

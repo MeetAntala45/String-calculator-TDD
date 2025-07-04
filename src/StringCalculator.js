3080a62b2c313195c87687a39c515bd9d072a7d1
MeetAntala45 <meetantala36@gmail.com>
2025-07-04 22:33:08 +0530

    [Test 4] Refactored Add() method to support dynamic delimiter parsing with regex

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -5,17 +5,25 @@ class StringCalculator {
     }
 
     let numbersPart = inputString;
-    let delimiter = /,|\n/;
+    let delimiters = [",", "\n"];
 
     if (inputString.startsWith("//")) {
-      const delimiterLineEnd = inputString.indexOf("\n");
-      delimiter = inputString[2];
-      numbersPart = inputString.slice(delimiterLineEnd + 1);
+      const delimiterEndIndex = inputString.indexOf("\n");
+      const delimiterDeclaration = inputString.slice(2, delimiterEndIndex);
+      delimiters = [delimiterDeclaration];
+      numbersPart = inputString.slice(delimiterEndIndex + 1);
     }
 
-    const numbers = numbersPart.split(new RegExp(delimiter)).map(Number);
+    const delimiterRegex = new RegExp(
+      delimiters.map((d) => escapeRegExp(d)).join("|")
+    );
+
+    const numbers = numbersPart.split(delimiterRegex).map(Number);
     return numbers.reduce((sum, num) => sum + num, 0);
   }
 }
 
+function escapeRegExp(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
 module.exports = StringCalculator;
